af82f5ab9cbd10ea3368a5d4dacd55fdacad7c04
MickyBe <bmikiyas@gmail.com>
2022-01-30 20:21:46 +0300

    runing error fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,9 +4,9 @@
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
 from models.city import City
 from models.place import Place
-from models.amenity import Amenity
 from models.review import Review
 from models.state import State
 
